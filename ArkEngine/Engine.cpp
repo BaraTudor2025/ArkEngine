35e67b6fdb8f6b2ba364680ef68b9db8914ec15f
Vector2025 <vector2025@gmail.com>
2019-10-16 11:41:42 +0300

    added imgui library

--- ArkEngine/Engine.cpp
+++ ArkEngine/Engine.cpp
@@ -1,10 +1,12 @@
-
 #include "Engine.hpp"
 #include "Scene.hpp"
 #include "Entity.hpp"
 #include "MessageBus.hpp"
 #include "State.hpp"
 #include "ResourceManager.hpp"
+
+#include <imgui.h>
+#include <imgui-SFML.h>
 
 const ComponentManager::ComponentMask& Entity::getComponentMask() { return manager->getComponentMaskOfEntity(*this); }
 
@@ -36,6 +38,8 @@ void ArkEngine::create(sf::VideoMode vm, std::string name, sf::Time fixedUpdateT
 	window.create(vm, name, sf::Style::Close | sf::Style::Resize, settings);
 	stateStack.messageBus = &messageBus;
 
+	ImGui::SFML::Init(window);
+
 	Resources::addHandler<sf::Texture>("textures", Resources::load_SFML_resource<sf::Texture>);
 	Resources::addHandler<sf::Font>("fonts", Resources::load_SFML_resource<sf::Font>);
 	Resources::addHandler<sf::Image>("imags", Resources::load_SFML_resource<sf::Image>);
@@ -49,10 +53,15 @@ void ArkEngine::updateEngine()
 	// handle events
 	sf::Event event;
 	while (window.pollEvent(event)) {
+
+		ImGui::SFML::ProcessEvent(event);
+
 		switch (event.type) {
+
 		case sf::Event::Closed:
 			window.close();
 			break;
+
 		case sf::Event::Resized: {
 			auto[x, y] = window.getSize();
 			auto aspectRatio = float(x) / float(y);
@@ -78,11 +87,14 @@ void ArkEngine::run()
 {
 	auto lag = sf::Time::Zero;
 
+	std::string windowTitle;
+	windowTitle.resize(50);
+
 	clock.restart();
+	sf::Color bgColor = sf::Color::Black;
+	float color[3] = {0.f, 0.f, 0.f};
 
 	while (window.isOpen()) {
-
-		window.clear(backGroundColor);
 
 		delta_time = clock.restart();
 
@@ -95,9 +107,37 @@ void ArkEngine::run()
 			updateEngine();
 		}
 #endif
+
+		ImGui::SFML::Update(window, deltaTime());
+
+		ImGui::Begin("Sample window"); // begin window
+
+		if (ImGui::ColorEdit3("Background color", color)) {
+			// this code gets called if color value changes, so
+			// the background color is upgraded automatically!
+			bgColor.r = static_cast<sf::Uint8>(color[0] * 255.f);
+			bgColor.g = static_cast<sf::Uint8>(color[1] * 255.f);
+			bgColor.b = static_cast<sf::Uint8>(color[2] * 255.f);
+		}
+
+		// Window title text edit
+		ImGui::InputText("Window title", windowTitle.data(), 20);
+
+		if (ImGui::Button("Update window title")) {
+			// this code gets if user clicks on the button
+			// yes, you could have written if(ImGui::InputText(...))
+			// but I do this to show how buttons work :)
+			window.setTitle(windowTitle);
+		}
+		ImGui::End(); // end window
+
+		window.clear(bgColor);
+
 		stateStack.render(window);
+		ImGui::SFML::Render(window);
 
 		window.display();
 	}
+	ImGui::SFML::Shutdown();
 }
 
